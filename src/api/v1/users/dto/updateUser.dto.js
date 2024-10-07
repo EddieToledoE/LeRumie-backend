694cf13455cc3fad43816dc8a6f72e6637e89550
EddieToledoE <Teddy_03@outlook.es>
2024-10-06 19:52:55 -0600

    Crud Completo

--- src/api/v1/users/dto/updateUser.dto.js
+++ src/api/v1/users/dto/updateUser.dto.js
@@ -4,6 +4,6 @@ const updateUserSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
   password: Joi.string().min(6),
-});
+}).min(1);
 
 module.exports = updateUserSchema;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,5 @@
 const userResource = require('../api/v1/users/user.resource');
 const bcrypt = require('bcrypt');
-// AQUI PODRIA PONER EL CIFRADO Y TODO ESO?
 
 const createUser = async (userData) => {
   const {password, ...rest} = userData;
@@ -13,7 +12,17 @@ const getUserById = async (userId) => {
   return await userResource.getUserById(userId);
 };
 
+const updateUserById = async (userId, updateData) => {
+  return await userResource.updateUser(userId, updateData);
+};
+
+const deleteUserById = async (userId) => {
+  return await userResource.deleteUser(userId);
+};
+
 module.exports = {
   createUser,
   getUserById,
+  updateUserById,
+  deleteUserById,
 };
