5d53bfde95db9f3ab553cc510a2fa6de425948a0
EddieToledoE <Teddy_03@outlook.es>
2025-01-25 19:00:16 -0600

    pending atributte

--- src/api/v1/tasks/dto/updateTask.dto.js
+++ src/api/v1/tasks/dto/updateTask.dto.js
@@ -4,6 +4,7 @@ const updateTaskSchema = Joi.object({
   title: Joi.string().min(3).max(30),
   description: Joi.string().min(6),
   status: Joi.string().valid('pending', 'in progress', 'completed'),
+  completed: Joi.boolean(),
 }).min(1);
 
 module.exports = updateTaskSchema;
--- src/models/task.js
+++ src/models/task.js
@@ -13,6 +13,11 @@ const TaskSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  status: {
+    type: String,
+    enum: ['pending', 'in progress', 'completed'],
+    default: 'pending',
+  },
   createdAt: {
     type: Date,
     default: Date.now,
